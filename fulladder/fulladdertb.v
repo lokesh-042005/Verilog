f21375cdcac2ff31fdc9137a6155079e3aa16438
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-16 13:20:42 +0000

    updated

--- fulladder/fulladdertb.v
+++ fulladder/fulladdertb.v
@@ -1,1 +1,30 @@
+module fulladdertb;
 
+reg A,B,Cin;
+wire Sum,Carry;
+
+fulladder uut(
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .Sum(Sum),
+    .Carry(Carry)
+);
+
+initial
+begin
+    $display("A B Cin | Sum Carry");
+    $monitor("%b %b %b | %b    %b",A,B,Cin,Sum,Carry);
+
+    A=0;B=0;Cin=0;
+    #10 A=0;B=0;Cin=1;
+    #10 A=0;B=1;Cin=0;
+    #10 A=0;B=1;Cin=1;
+    #10 A=1;B=0;Cin=0;
+    #10 A=1;B=0;Cin=1;
+    #10 A=1;B=1;Cin=0;
+    #10 A=1;B=1;Cin=1;
+    #10 $finish;
+end
+
+endmodule
