89b5b61d20b38cc2f2df533b15f40b012e462d03
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-18 16:09:53 +0000

    4 bit carry look ahead adder updated

---
+++ Day2/carry_look_ahead_adder/carry_look_ahead_addertb.v
@@ -0,0 +1,35 @@
+module carry_look_ahead_addertb;
+
+reg  [3:0] A;
+reg  [3:0] B;
+reg  Cin;
+
+wire [3:0] Sum;
+wire Cout;
+
+
+carry_look_ahead_adder uut (
+    .A(A),
+    .B(B),
+    .Cin(Cin),
+    .Sum(Sum),
+    .Cout(Cout)
+);
+
+initial
+begin
+    $display("Time\tA\tB\tCin\tSum\tCout");
+    $monitor("%0t\t%b\t%b\t%b\t%b\t%b",
+             $time, A, B, Cin, Sum, Cout);
+
+    A = 4'b0000; B = 4'b0000; Cin = 0; #10;
+    A = 4'b0001; B = 4'b0010; Cin = 0; #10;
+    A = 4'b0101; B = 4'b0011; Cin = 0; #10;
+    A = 4'b0111; B = 4'b1000; Cin = 0; #10;
+    A = 4'b1111; B = 4'b0001; Cin = 0; #10;
+    A = 4'b1010; B = 4'b0101; Cin = 1; #10;
+
+    $finish;
+end
+
+endmodule
