8d074ef7337b64cfc02a44dba2abdb6451bce88f
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-21 12:20:05 +0000

    synchronous counter updated

---
+++ Day5/Sequential_circuits/synchronous_counter/synchronous_countertb.v
@@ -0,0 +1,30 @@
+module synchronous_countertb;
+
+reg CLK;
+wire [3:0] Q0;
+
+synchronous_counter uut(
+    .CLK(CLK),
+    .Q0(Q0)
+);
+
+always
+begin
+    #5 CLK = ~CLK;
+end
+
+initial
+begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, uut);
+
+    CLK = 0;
+
+    $display("Time\tCLK\tQ0");
+    $monitor("%0t\t%b\t%b",$time,CLK,Q0);
+
+    #170;
+    $finish;
+end
+
+endmodule
