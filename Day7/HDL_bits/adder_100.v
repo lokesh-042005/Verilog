0a0472ae8f299acbff334c70e9f327701b1abc9a
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-23 10:21:46 +0000

    adder 100 updated

---
+++ Day7/HDL_bits/adder_100.v
@@ -0,0 +1,50 @@
+module top_module(
+    input [99:0] a,
+    input [99:0] b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum
+);
+
+    genvar i;
+
+    generate
+        for(i = 0; i < 100; i = i + 1) begin : FA
+
+            if(i == 0) begin
+                full_adder fa (
+                    .a(a[i]),
+                    .b(b[i]),
+                    .cin(cin),
+                    .sum(sum[i]),
+                    .cout(cout[i])
+                );
+            end
+            else begin
+                full_adder fa (
+                    .a(a[i]),
+                    .b(b[i]),
+                    .cin(cout[i-1]),
+                    .sum(sum[i]),
+                    .cout(cout[i])
+                );
+            end
+
+        end
+    endgenerate
+
+endmodule
+
+
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+
+    assign sum  = a ^ b ^ cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+
+endmodule
