9d671f615b54b560485fd1df132bdec977020f7e
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-29 11:03:35 +0000

    nbit shift register updated

---
+++ Day12/HDL_bits/nbit_shiftregister.v
@@ -0,0 +1,62 @@
+module top_module (
+    input [3:0] SW,
+    input [3:0] KEY,
+    output [3:0] LEDR
+);
+
+    MUXDFF u0 (
+        .clk(KEY[0]),
+        .w(KEY[3]),
+        .R(SW[3]),
+        .E(KEY[1]),
+        .L(KEY[2]),
+        .Q(LEDR[3])
+    );
+
+    MUXDFF u1 (
+        .clk(KEY[0]),
+        .w(LEDR[3]),
+        .R(SW[2]),
+        .E(KEY[1]),
+        .L(KEY[2]),
+        .Q(LEDR[2])
+    );
+
+    MUXDFF u2 (
+        .clk(KEY[0]),
+        .w(LEDR[2]),
+        .R(SW[1]),
+        .E(KEY[1]),
+        .L(KEY[2]),
+        .Q(LEDR[1])
+    );
+
+    MUXDFF u3 (
+        .clk(KEY[0]),
+        .w(LEDR[1]),
+        .R(SW[0]),
+        .E(KEY[1]),
+        .L(KEY[2]),
+        .Q(LEDR[0])
+    );
+
+endmodule
+
+
+module MUXDFF (
+    input clk,
+    input w,
+    input R,
+    input E,
+    input L,
+    output reg Q
+);
+
+always @(posedge clk) begin
+    if (L)
+        Q <= R;
+    else if (E)
+        Q <= w;
+end
+
+endmodule
