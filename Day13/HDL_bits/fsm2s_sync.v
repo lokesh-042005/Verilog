318062202d9de490690c092d40c45273aa7f6084
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-30 06:33:46 +0000

    fsm2s synchronous updated

---
+++ Day13/HDL_bits/fsm2s_sync.v
@@ -0,0 +1,46 @@
+module top_module(
+    input clk,
+    input reset,    
+    input j,
+    input k,
+    output out); 
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        
+        case(state)
+            OFF: begin
+                case({j,k})
+                    2'b00: next_state = OFF;
+                    2'b01: next_state = OFF;
+                    2'b10: next_state = ON;
+                    2'b11: next_state = ON;
+                endcase
+            end
+
+            ON: begin
+                case({j,k})
+                    2'b00: next_state = ON;
+                    2'b01: next_state = OFF;
+                    2'b10: next_state = ON;
+                    2'b11: next_state = OFF;
+                endcase
+            end
+        endcase
+    end
+
+    always @(posedge clk) begin
+        
+         if (reset)
+            state <= OFF;
+        else
+            state <= next_state;
+    end
+
+    
+    assign out = (state == ON);
+
+endmodule
+
