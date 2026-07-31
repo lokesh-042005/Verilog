9fec0343e50e39eb134d033079824e571c72ba1c
lokesh-042005 <lokeshvelu17@gmail.com>
2026-07-31 09:51:38 +0000

    sim circuit 10 updated

---
+++ Day14/HDL_bits/sim_circuit10.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input a,
+    input b,
+    output q,
+    output state  );
+    
+	always @(posedge clk)
+        if (a == b)
+            state <= a;
+
+    assign q = a ^ b ^ state;
+    
+endmodule
+
